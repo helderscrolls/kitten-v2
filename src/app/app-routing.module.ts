84608c5d479b6624e5d10569b3013c5e2bf1c792
Helder Nascimento <helder@lifeaz.fr>
2020-03-02 14:55:06 +0100

    feature(config): add angular material, add routing, add testing material card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'register-item',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/itemslist/itemslist.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-itemslist',
+  templateUrl: './itemslist.component.html',
+  styleUrls: ['./itemslist.component.scss']
+})
+export class ItemslistComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
